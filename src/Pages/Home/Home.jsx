af12efc633e0be2a99cb0f322b4b534cb6367b6e
Sahed96 <ninja.sahedkhan@gmail.com>
2025-01-30 13:27:53 +0600

    Before Tailwind upgrade

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from '../../components/Banner/Banner';
+import Services from '../../components/Services';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Services />
     </div>
   );
 };
---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+const Services = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold mx-auto mt-4">এখানে সকল প্রকার <span className="hover:text-red-500 hover:bg-slate-300 rounded-3xl">সার্ভিস</span> পাওয়া যায়</h1>
+    </div>
+  );
+};
+
+export default Services;
